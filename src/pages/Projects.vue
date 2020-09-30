e67637692ec73cd11a417bb556d35836cc55d831
Yogendra Yadav <yogicse12@gmail.com>
2020-09-30 09:22:06 +0100

    Project and resume

---
+++ src/pages/Projects.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Projects</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/Resume.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Resume</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
